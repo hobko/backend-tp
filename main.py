d75e38acbac673df040c288ba613b122671e284f
hodkyy@gmail.com <Hodky1597>
2024-01-19 16:17:01 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,70 @@
+import os
+import subprocess
+from pathlib import Path
+
+from scripts.csvTogeojsonTogpxA import csv_to_gpx
+from fastapi import FastAPI, UploadFile, File, HTTPException
+from fastapi.responses import FileResponse
+
+from starlette.middleware.cors import CORSMiddleware
+from starlette.responses import JSONResponse
+
+app = FastAPI()
+
+origins = ["http://localhost:4200"]
+cur: Path = Path(__file__).parent
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get('/api/getgpx/{filename}')
+async def get_gpx(filename: str):
+    try:
+        # Replace this with the path to your GPX files directory
+        gpx_file_path = cur / "storage/geojson-cleaned" / filename
+
+        # Send the GPX file as a response
+        return FileResponse(gpx_file_path, media_type='application/gpx+xml', filename=filename)
+    except FileNotFoundError:
+        raise HTTPException(status_code=404, detail='File not found')
+
+@app.get("/api/getfiles")
+async def get_files():
+    folder_path = cur / "storage/geojson"  # Replace with the actual path to your GPX folder
+
+    try:
+        # List all files in the folder
+        files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
+        return {"files": files}
+    except Exception as e:
+        return {"error": f"Failed to retrieve files. Error: {str(e)}"}
+
+
+@app.post("/api/upload")
+async def upload_file(file: UploadFile = File(...)):
+    # Path to the uploaded file
+    uploaded_file_path = cur / "storage/uploads" / file.filename
+    with open(uploaded_file_path, 'wb') as uploaded_file:
+        uploaded_file.write(file.file.read())
+    csv_to_gpx(file.filename)
+    return {"message": "File uploaded and processed successfully"}
+
+
+@app.get("/api/hello")
+def hello_world():
+    return {"message": "Hello, Worssssld!"}
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
---
+++ scripts/csvTogeojsonTogpxA.py
@@ -0,0 +1,119 @@
+import csv
+import os
+
+from geojson import Feature, FeatureCollection, Point
+from pathlib import Path
+import re
+import geopandas as gpd
+import gpxpy
+import gpxpy.gpx
+import json
+
+
+def make_geojson_from_data(input_file_path, output_file_path):
+    features = []
+
+    with open(input_file_path, mode='r', newline='') as input_file:
+        csv_reader = csv.reader(input_file)
+        header = next(csv_reader)
+
+        lat_column_index = 4
+        lon_column_index = 5
+
+        for row in csv_reader:
+            lat_format = re.sub(r'.$', '', row[lat_column_index])
+            lon_format = re.sub(r'.$', '', row[lon_column_index])
+            lat = float(lat_format)
+            lon = float(lon_format)
+
+            point = Point((lon, lat))
+
+            properties = {
+                'date': row[2],
+                'time': row[3],
+                'speed': row[7]
+            }
+            feature = Feature(geometry=point, properties=properties)
+            features.append(feature)
+
+    feature_collection = FeatureCollection(features)
+
+    with open(output_file_path, 'w') as output_file:
+        output_file.write(str(feature_collection))
+
+
+def validate_and_clean_geojson(input_file, output_file):
+    gdf = gpd.read_file(input_file)
+
+    # Filter out invalid geometries
+    gdf = gdf[gdf.geometry.is_valid]
+
+    # Filter out rows with missing or invalid properties
+    valid_properties = ["date", "time", "speed"]
+    gdf = gdf[gdf.apply(lambda row: all(prop in row for prop in valid_properties), axis=1)]
+
+    # Save the cleaned data to a new GeoJSON file
+    if output_file:
+        gdf.to_file(output_file, driver='GeoJSON')
+
+    return gdf
+
+
+def geojson_to_gpx(input_geojson, output_gpx):
+    with open(input_geojson, 'r') as geojson_file:
+        data = json.load(geojson_file)
+
+    gpx = gpxpy.gpx.GPX()
+    track = gpxpy.gpx.GPXTrack()
+    segment = gpxpy.gpx.GPXTrackSegment()
+
+    for feature in data['features']:
+        geometry_type = feature['geometry']['type']
+        coordinates = feature['geometry']['coordinates']
+
+        if geometry_type == 'Point':
+            lat, lon = coordinates[1], coordinates[0]  # GeoJSON uses [lon, lat] order
+            segment.points.append(gpxpy.gpx.GPXTrackPoint(lat, lon))
+        elif geometry_type == 'LineString':
+            for point in coordinates:
+                lat, lon = point[1], point[0]  # GeoJSON uses [lon, lat] order
+                segment.points.append(gpxpy.gpx.GPXTrackPoint(lat, lon))
+        elif geometry_type == 'MultiLineString':
+            for line_string in coordinates:
+                for point in line_string:
+                    lat, lon = point[1], point[0]  # GeoJSON uses [lon, lat] order
+                    segment.points.append(gpxpy.gpx.GPXTrackPoint(lat, lon))
+
+    track.segments.append(segment)
+    gpx.tracks.append(track)
+
+    with open(output_gpx, 'w') as gpx_file:
+        gpx_file.write(gpx.to_xml())
+
+
+def remove_file_extension(filename):
+    root, extension = os.path.splitext(filename)
+    return root
+
+
+def csv_to_gpx(input_file_name):
+    cur: Path = Path(__file__).parent.parent
+    input_file_name_wo_extension = remove_file_extension(input_file_name)
+    intermediate_file_name = input_file_name_wo_extension + ".geojson"
+    # Step 1: Generate GeoJSON from CSV
+    input_csv_file = cur / "storage/uploads" / input_file_name
+    intermediate_geojson_file = cur / "storage/geojson" / intermediate_file_name
+    make_geojson_from_data(input_csv_file, intermediate_geojson_file)
+    print(f'CSV file "{input_csv_file}" has been converted to GeoJSON and saved as "{intermediate_file_name}".')
+
+    # Step 2: Validate and clean the GeoJSON
+    input_geojson_file = intermediate_geojson_file
+    output_cleaned_geojson_file = cur / "storage/geojson-cleaned" / intermediate_file_name
+    validate_and_clean_geojson(input_geojson_file, output_cleaned_geojson_file)
+    print(
+        f'GeoJSON file "{input_geojson_file}" has been validated and cleaned, saved as "{output_cleaned_geojson_file}".')
+
+    # Step 3: Transform Geojson to GPX
+    output_gpx_file = input_file_name_wo_extension + ".gpx"
+    output_gpx = cur / "storage/gpx" / output_gpx_file
+    geojson_to_gpx(output_cleaned_geojson_file, output_gpx)
